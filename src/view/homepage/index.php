2d63825ec3cc66ab34788296e5c20fcca3b37328
Luca Shin <zhongmingzi@gmail.com>
2024-12-01 17:01:17 +0800

    fixedSomething

--- src/view/homepage/index.php
+++ src/view/homepage/index.php
@@ -31,7 +31,7 @@ $qrcodeImage = '二维码图片路径';
             text-align: center;
             padding: 20px;
             margin: 0;
-            font-size: small;
+            font-size: Small;
         }
         .profile {
             margin-bottom: 20px;
@@ -51,7 +51,7 @@ $qrcodeImage = '二维码图片路径';
         }
         .username {
             font-weight: bold;
-            font-size: normal;
+            font-size: Normal;
             margin-top: 10px;
         }
         .images {
