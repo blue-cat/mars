8056487c5473044d21d706402603c911ce2b1928
Luca Shin <zhongmingzi@gmail.com>
2024-12-02 03:09:33 +0800

    fixedSomething

--- src/app/Api/Homepage/Homepage.php
+++ src/app/Api/Homepage/Homepage.php
@@ -75,7 +75,6 @@ class Homepage extends Api {
 
             // 拉取用户的二维码
             $qrcodeImage = $this->domain. '/'. $media->getMediaByObjIdAndOrder(2, $user_id, 0)['dir'];
-            print_r($media->getMediaByObjIdAndOrder(2, $user_id, 0));
         }
 
         include(API_ROOT . '/src/view/homepage/index.php');
