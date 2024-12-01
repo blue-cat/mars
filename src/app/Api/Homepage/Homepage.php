423c3999f3801771ec36beff2aa5f42521eedb8c
Luca Shin <zhongmingzi@gmail.com>
2024-12-01 20:56:28 +0800

    fixedSomething

--- src/app/Api/Homepage/Homepage.php
+++ src/app/Api/Homepage/Homepage.php
@@ -20,6 +20,7 @@ class Homepage extends Api {
         if (!$user_id) {
             // 给个简单样式，文字页面上下左右居中显示，并使用灰色文字，使用16号字体
             echo "<style>body{margin: 0;padding: 0;text-align: center;font-size: 16px;color: #999999;}</style>";
+            echo "<div class='error'>用户不存在</div>";
             exit(0);
         }
 
