f46a7d407a62316a29d358c0c4acfd083bdebc7b
Luca Shin <zhongmingzi@gmail.com>
2024-12-04 01:06:53 +0800

    fixedSomething

--- src/app/Api/Homepage/Homepage.php
+++ src/app/Api/Homepage/Homepage.php
@@ -108,7 +108,6 @@ class Homepage extends Api {
         }
 
         //检测敏感词
-        return Filter::check($data);
         if (!Filter::check($data)) {
             throw new \Exception('包含违规内容！', 422);
         }
