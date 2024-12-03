e303de9364a8c53144b0ce8f1e8dffceb85b35c1
Luca Shin <zhongmingzi@gmail.com>
2024-12-04 00:58:22 +0800

    fixedSomething

--- src/app/Domain/Misc/Filter.php
+++ src/app/Domain/Misc/Filter.php
@@ -8,7 +8,7 @@ class Filter
 {
     //现在有个接口http://localhost:8800/check?text=文本，如果包含敏感词，返回true，否则返回false
     //请通过访问这个接口来获取结果，并在结果中看到是否有敏感词。
-    private static $url = 'http://localhost:8800/check?text=';
+    private static $url = 'http://172.17.0.1:8800/check?text=';
 
     public static function check($text)
     {
