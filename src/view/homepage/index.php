0e774c30ea81abdb1bdc2d615309a27816a046d8
Luca Shin <zhongmingzi@gmail.com>
2024-12-03 22:15:27 +0800

    fixedSomething

--- src/view/homepage/index.php
+++ src/view/homepage/index.php
@@ -331,6 +331,7 @@ list($appid, $h5AppSecret) = array_values(\PhalApi\DI()->config->get('vendor.wei
             input.accept = 'image/*';
 
             input.addEventListener('change', function (event) {
+                alert("Uploading image");
                 const file = event.target.files[0];
                 if (!file) {
                     alert('请选择图片');
