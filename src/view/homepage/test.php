613894e4d43a8540a2da538b6f07f8f5aa500dc8
Luca Shin <zhongmingzi@gmail.com>
2024-12-09 23:53:28 +0800

    fixedSomething

--- src/view/homepage/test.php
+++ src/view/homepage/test.php
@@ -307,7 +307,7 @@
         <a href="#" id="create-homepage-btn">登录我的Homepage</a> | 
         <a href="https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzkxMDYzMTM0NA==&scene=110#wechat_redirect
 ">火星殖民计划</a> |
-        <button id="share-image-btn">分享为图片</button>
+        <a href="javascript:void(0);" id="share-image-btn">分享为图片</a>
     </div>
     <script src="https://res.wx.qq.com/open/js/jweixin-1.6.0.js"></script>
     <script src="https://cdn.bootcdn.net/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
